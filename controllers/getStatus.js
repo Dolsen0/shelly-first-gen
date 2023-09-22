46fa64a88955a66e3e7938342310c3f85048c029
dolsen0 <derek.olsen@gmail.com>
2023-09-21 20:43:55 -0400

    wrote logging function to determine motion, vibration, and time data from motion sensor

--- controllers/getStatus.js
+++ controllers/getStatus.js
@@ -3,7 +3,9 @@ import axios from "axios";
 export default async function getStatus(req, res){
     await axios.get(`http://shelly${process.env.DEVICE}-${process.env.MAC}/status`)
     .then((response) => {
+      const sensor = response.data.sensor;
       res.send(response.data);
+      console.log(JSON.stringify(sensor, null, 2));
     })
     .then(() => {
         console.log("Success");
